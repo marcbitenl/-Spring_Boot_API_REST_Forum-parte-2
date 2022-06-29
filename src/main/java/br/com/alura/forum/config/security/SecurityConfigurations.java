58621a555ac1decde554c4b15d94f4f85c081838
marcb <marcbitenl@gmail.com>
2022-06-28 21:26:57 -0300

    Liberando acesso aos endpoints

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,13 +1,40 @@
 package br.com.alura.forum.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @SuppressWarnings("deprecation")
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	
+	
+	//configurações de autenticação acesso login
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	} 
+	
+	//configurações de autorização
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests()
+		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+		
+	}
+
+	
+	
+	//configuração de recursos estáticos(js,css, imagens, etc.)
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		
+	}
 
 }
